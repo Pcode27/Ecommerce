eec9864f1db1c6221cedd3c7209741d87e92641d
Pcode27 <92858414+Pcode27@users.noreply.github.com>
2022-02-16 14:24:43 +0300

    Add files via upload

---
+++ About.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<?php include 'includes/session.php'; ?>
+
+<head>
+    <title>
+        About Us
+    </title>
+    
+</head>
+
+<body onload="themeColor('<?php echo $gender ?>')">
+    <!-- Import Fixed Menu Sidebar -->
+    <?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+	<?php include 'includes/navbar.php'; ?>
+	 
+	  <div class="content-wrapper">
+	    <div class="container">
+        <section class="content">
+
+                <h1>About Us</h1>
+                <hr>
+
+
+                <p>
+                    FeetBit is a startup company that provides footwear retailing for customers within Kenya.
+                    As the pandemic hit, the retail industry suffered from a lack of customers. Thus, our company
+                    provides online merchants and goods which allow customers to make purchases online without leaving
+                    their house. Partnerships with brands that are in high demand among customers are established to
+                    provide a large variety of products for our local customers. These partnerships also mean that
+                    exclusive products are available in our store.
+                    </br></br>
+                    Our company aspires to be the number 1 footwear retailing store in Kenya. We will continue to
+                    establish trust towards our loyal customers and satisfy them however we could to achieve our
+                    ultimate goal.
+                    </br></br>
+                    OUR GOAL: Every Customer Delighted!
+                </p>
+            </div>
+
+            
+                
+            
+
+    
+	        		<?php include 'includes/sidebar.php'; ?>
+	        	</div>
+	        </div>
+	      </section>
+	     
+	    </div>
+	  </div>
+  
+  	<?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+
+</html>
